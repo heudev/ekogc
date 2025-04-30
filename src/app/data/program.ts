38dc290fca4ff67a17840863ce0610896df0a941
heudev <hulkienesuysal@gmail.com>
2025-04-30 22:55:27 +0300

    update program

--- src/app/data/program.ts
+++ src/app/data/program.ts
@@ -2,38 +2,37 @@ export const program = [
     {
         day: "1. Gün - 3 Mayıs",
         schedule: [
-            { time: "09:00", activity: "Kayıt" },
-            { time: "10:00", activity: "Açılış Konuşmaları" },
-            { time: "11:00", activity: "Mola" },
-            { time: "11:30", activity: "Oturum 1" },
-            { time: "12:30", activity: "Mola" },
-            { time: "13:30", activity: "Oturum 2" },
-            { time: "14:30", activity: "Mola" },
-            { time: "15:00", activity: "Oturum 3" },
+            { time: "09:30", activity: "Kayıt" },
+            { time: "10:00", activity: "Açılış" },
+            { time: "10:30", activity: "Mola" },
+            { time: "10:45", activity: "1. Oturum" },
+            { time: "11:45", activity: "Mola" },
+            { time: "12:00", activity: "2. Oturum" },
+            { time: "13:00", activity: "Öğle arası" },
+            { time: "14:00", activity: "Social network" },
+            { time: "15:00", activity: "3. Oturum" },
             { time: "16:00", activity: "Mola" },
-            { time: "16:30", activity: "Oturum 4" },
-            { time: "17:30", activity: "Mola" },
-            { time: "18:00", activity: "Oturum 5" }
+            { time: "16:15", activity: "4. Oturum" },
+            { time: "17:15", activity: "Mola" },
+            { time: "17:30", activity: "5. Oturum" },
+            { time: "18:00", activity: "Gün sonu" }
         ]
     },
     {
         day: "2. Gün - 4 Mayıs",
         schedule: [
-            { time: "09:00", activity: "Oturum 6" },
+            { time: "09:00", activity: "6. Oturum" },
             { time: "10:00", activity: "Mola" },
-            { time: "10:15", activity: "Oturum 7" },
-            { time: "11:15", activity: "Mola" },
-            { time: "11:30", activity: "Oturum 8" },
-            { time: "12:30", activity: "Öğle Yemeği" },
-            { time: "13:30", activity: "Oturum 9" },
-            { time: "14:30", activity: "Mola" },
-            { time: "14:45", activity: "Oturum 10" },
-            { time: "15:45", activity: "Mola" },
-            { time: "16:00", activity: "Oturum 11" },
-            { time: "17:00", activity: "Mola" },
-            { time: "17:15", activity: "Oturum 12" },
-            { time: "18:15", activity: "Mola" },
-            { time: "18:30", activity: "Oturum 13" }
+            { time: "10:30", activity: "7. Oturum" },
+            { time: "11:30", activity: "Mola" },
+            { time: "11:45", activity: "8. Oturum" },
+            { time: "12:45", activity: "Öğle Yemeği" },
+            { time: "13:45", activity: "Genel kurul oturumu 1" },
+            { time: "15:00", activity: "Mola" },
+            { time: "15:30", activity: "Genel kurul oturumu 2" },
+            { time: "16:45", activity: "Mola" },
+            { time: "17:00", activity: "Kapanış" },
+            { time: "17:30", activity: "Gün sonu" }
         ]
     }
 ];
